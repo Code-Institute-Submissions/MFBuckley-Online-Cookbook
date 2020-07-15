a48c9a157a92c259563abf289b37383be7643986
MFBuckley <m.buckleyeos@gmail.com>
2020-07-15 07:44:11 +0000

    Added routes in app.py to all html files in templates folder to be able to Create Read Update Delete info from database.

--- app.py
+++ app.py
@@ -8,15 +8,27 @@ app = Flask(__name__)
 
 # Connect to database
 app.config["MONGO_DBNAME"] = 'online_cooking'
-app.config["MONGO_URI"] = 'mongodb://username:pw@myfirstcluster-shard-00-00-riwiq.gcp.mongodb.net:27017,myfirstcluster-shard-00-01-riwiq.gcp.mongodb.net:27017,myfirstcluster-shard-00-02-riwiq.gcp.mongodb.net:27017/online_cooking?ssl=true&replicaSet=myFirstCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb://carrot:WasUpDOC@myfirstcluster-shard-00-00-riwiq.gcp.mongodb.net:27017,myfirstcluster-shard-00-01-riwiq.gcp.mongodb.net:27017,myfirstcluster-shard-00-02-riwiq.gcp.mongodb.net:27017/online_cooking?ssl=true&replicaSet=myFirstCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
 
-# create instance of PyMongo
+# Create instance of PyMongo
 mongo = PyMongo(app)
 
 @app.route('/')
+
+# Read Recipes
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("all_recipes.html", recipes=mongo.db.recipes.find())
+
+# Create/Add recipes to database
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
+
+# Update Recipe
+@app.route('/update_recipe')
+def update_recipe():
+    return render_template("edit.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
